0f54f2a6ec877bf86dbbd7cc1e486585d637a2e1
Messias Junior <messias.junior@tui.co.uk>
2020-12-29 08:33:40 +0000

    replace a binding by a fake implementation

--- app/src/androidTest/java/com/example/hiltsampleapp/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/hiltsampleapp/MainActivityTest.kt
@@ -5,24 +5,41 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import com.example.hiltsampleapp.datasource.local.MainLocalDataSourceImpl
+import com.example.hiltsampleapp.datasource.DataSourceModule
+import com.example.hiltsampleapp.datasource.api.MainRemoteDataSource
+import com.example.hiltsampleapp.datasource.local.MainLocalDataSource
+import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
 import org.junit.Rule
 import org.junit.Test
 
+@UninstallModules(DataSourceModule::class)
 @HiltAndroidTest
-class MainActivityTest{
+class MainActivityTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
     @get:Rule
     var activityRule = ActivityScenarioRule(MainActivity::class.java)
 
+    @BindValue
+    @JvmField
+    val fakeLocalDataSource: MainLocalDataSource = object : MainLocalDataSource {
+        override suspend fun getWelcomeText() = TEST_WELCOME_TEXT
+    }
+
+    @BindValue
+    @JvmField
+    val fakeRemoteDataSource: MainRemoteDataSource = object : MainRemoteDataSource {}
+
     @Test
-    fun shouldShowTheWelcomeText(){
-        Thread.sleep(1000)
-        onView(withId(R.id.welcomeMessageTextView))
-            .check(matches(withText(MainLocalDataSourceImpl.WELCOME_TEXT)))
+    fun shouldShowTheWelcomeText() {
+        onView(withId(R.id.welcomeMessageTextView)).check(matches(withText(TEST_WELCOME_TEXT)))
+    }
+
+    companion object {
+        private const val TEST_WELCOME_TEXT = "Injection of mock works!"
     }
 }
